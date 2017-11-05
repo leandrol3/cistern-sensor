d21556b04dc6ead5e01d36f68a0e311dbc75ee78
Leandro Lopes <leandro.lopes@bmind.ai>
2017-11-05 19:47:17 -0200

    Add files
    Version 1.0

--- cisterna_sensor/cisterna_sensor.ino
+++ cisterna_sensor/cisterna_sensor.ino
@@ -9,42 +9,51 @@
 #define ECHO_PIN     12  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
+#define EMPTY 170 // Height to measure the capacity when is empty in centimeters
+#define FULL 10 // Height to measure the capacity when is full in centimeters
+
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
   
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
   
-  //Determines all ports to Bar graph
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);    
-  pinMode(11, OUTPUT);
-  
+  //Determines all ports to Bar graph from 2 to 12
+  for (int i = 2; i < 12; i++) {
+    pinMode(i, OUTPUT);
+  }
 }
 
 void loop() {
+  int distance;  //load distance from ultrasonic sensor
+  int level; //level of bar graph to print between 170 (max height - empty) to 10 (min height - full)
+  distance = sonar.ping_cm();
   delay(150);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-  Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
+  level = map(distance, FULL, EMPTY, 0, 10);  //Maps distance to level from 0 to 10
+ 
+  if (distance != 0) {   // Somente ativa se o valor for diferente de 0 
+     //If debug on
+    Serial.print("Ping: ");
+    Serial.print(distance); // Send ping, get distance in cm and print result (0 = outside set distance range)
+    Serial.println("cm");
+    Serial.println(level);
+    
+    setbargraph(level);
 
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, HIGH);
-  digitalWrite(10, HIGH);
-  digitalWrite(11, HIGH);
-
+  }
   
 }
+
+void setbargraph(int level) {
+  //Acende somente até o nivel passado na variavel qtde
+  for (int i = 0; i < 11; i++) {
+     if (i < level) {
+        digitalWrite(i+2, LOW);
+     }
+     else {
+        digitalWrite(i+2, HIGH);
+     }
+  }
+}
+
+
