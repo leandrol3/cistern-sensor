4d200008be30ec9a39f925efdc1a8194ebeade8b
Leandro Lopes <leandro.lopes@bmind.ai>
2018-08-19 18:36:34 -0300

    New frizting- reducao do tamnanho da placa

--- cisterna_sensor/cisterna_sensor.ino
+++ cisterna_sensor/cisterna_sensor.ino
@@ -29,17 +29,19 @@ void loop() {
   int distance;  //load distance from ultrasonic sensor
   int level; //level of bar graph to print between 154 (max height - empty) to 4 (min height - full)
   distance = sonar.ping_cm();
-  delay(150);                     // Wait 150ms between pings (about 8 pings/sec). 29ms should be the shortest delay between pings.
+  delay(350);                     // Wait 150ms between pings (about 8 pings/sec). 29ms should be the shortest delay between pings.
   //If debug on
 //  distance = 10;
-  
+  Serial.print("Distance: "); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println(distance); // Send ping, get distance in cm and print result (0 = outside set distance range)
+
   level = map(distance, FULL, EMPTY, 0, 10);  //Maps distance to level from 0 to 10
   if (distance != 0) {   // Somente ativa se o valor for diferente de 0 
 //    If debug on
-//    Serial.print("Ping: ");
-//    Serial.print(distance); // Send ping, get distance in cm and print result (0 = outside set distance range)
-//    Serial.println("cm");
-//    Serial.println(level);
+    Serial.print("Ping: ");
+    Serial.print(distance); // Send ping, get distance in cm and print result (0 = outside set distance range)
+    Serial.println("cm");
+    Serial.println(level);
     setbargraph(level);
   } 
 }
